6b6eed4fa5cdece8b43a3ca6391b531ab38d7ff6
Venryx <venryx@gmail.com>
2019-11-15 03:34:20 -0800

    * Fixed that the plugin would fail to apply in certain situations. (where there are copies of eslint not at the project root)

--- src/only-warn.js
+++ src/only-warn.js
@@ -1,20 +1,45 @@
-const eslint = require('eslint')
+const path = require("path")
 
-const LinterPrototype = (eslint.Linter && eslint.Linter.prototype) || eslint.linter
-const originalVerify = LinterPrototype.verify
+const needle = `${path.sep}node_modules${path.sep}eslint${path.sep}`
+function getLinters() {
+  if (linters.length == 0) {
+    const eslintPaths_withParams = ["eslint"].concat(Object.keys(require.cache).filter(id => id.includes(needle)))
+    const eslintPaths = new Set(eslintPaths_withParams.map(id => id.slice(0, id.indexOf(needle) + needle.length)))
+    for (const eslintPath of eslintPaths) {
+      try {
+        const linter = require(eslintPath).Linter
+        if (linter) {
+            linters.push(linter)
+        }
+      } catch (error) {
+        if (error.code !== "MODULE_NOT_FOUND") {
+          throw error
+        }
+      }
+    }
+  }
+  return linters
+}
+
+const linters = []
+const originalVerifies = []
 
 /**
  * Patch the verify method and downgrade the errors to warnings.
  */
 function enable () {
-  LinterPrototype.verify = function () {
-    const messages = originalVerify.apply(this, arguments)
-    messages.forEach(message => {
-      if (message.severity === 2) {
-        message.severity = 1
-      }
-    })
-    return messages
+  for (const [index, Linter] of getLinters().entries()) {
+    const originalVerify = Linter.prototype._verifyWithoutProcessors;
+	  originalVerifies[index] = originalVerify;
+    Linter.prototype["_verifyWithoutProcessors"] = function() {
+      const messages = originalVerify.apply(this, arguments)
+      messages.forEach(message => {
+        if (message.severity === 2) {
+          message.severity = 1
+        }
+      })
+      return messages
+    };
   }
 }
 
@@ -22,7 +47,9 @@ function enable () {
  * Remove the patch
  */
 function disable () {
-  LinterPrototype.verify = originalVerify
+	for (const [index, Linter] of getLinters().entries()) {
+    Linter.prototype["_verifyWithoutProcessors"] = originalVerifies[index];
+  }
 }
 
 enable()
