053dcfdff80dbef6b20632f7fbc85d72f751497c
Grant Forsythe <grantwforsythe@gmail.com>
2024-05-10 11:54:22 -0400

    style: add prettier config
    Add a prettier config as user styles might overwrite Prettier's default
    styles.

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  bracketSpacing: true,
+};
